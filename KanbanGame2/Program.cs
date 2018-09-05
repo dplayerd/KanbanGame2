c46a9ab95170a084a59cdbc198259b18ee285839
brian <uponcheng@yahoo.com.tw>
2018-09-05 20:58:37 +0800

    1.read data 2.Deserialize

--- KanbanGame2/Program.cs
+++ KanbanGame2/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
 
 namespace KanbanGame2
 {
@@ -10,9 +13,23 @@ namespace KanbanGame2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World.");
-            Console.WriteLine("Hello world2.");
-            Console.WriteLine("Hello world3.");
+            DeserializeObject();
+        }
+        public static void DeserializeObject()
+        {
+            //JSON字串
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000352-001");
+            StreamReader reader = new StreamReader(stream);
+            String content = reader.ReadToEnd();
+            string json = reader.ReadToEnd();
+
+            //轉成物件
+            List<OpenDate> items = JsonConvert.DeserializeObject<List<OpenDate>>(json);        }
+
+        public class OpenDate
+        {
+
         }
     }
 }
