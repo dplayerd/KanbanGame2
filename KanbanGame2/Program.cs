53396fe85f6c9b11f8067eb4bf02bafb89dea20d
brian <uponcheng@yahoo.com.tw>
2018-09-05 21:21:03 +0800

    add DTO but can not work...

--- KanbanGame2/Program.cs
+++ KanbanGame2/Program.cs
@@ -22,14 +22,50 @@ namespace KanbanGame2
             Stream stream = client.OpenRead("http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000352-001");
             StreamReader reader = new StreamReader(stream);
             String content = reader.ReadToEnd();
-            string json = reader.ReadToEnd();
 
             //轉成物件
-            List<OpenDate> items = JsonConvert.DeserializeObject<List<OpenDate>>(json);        }
-
+            List<OpenDate> items = JsonConvert.DeserializeObject<List<OpenDate>>(content);
+        }
+        
+        
         public class OpenDate
         {
-
+            public bool success { get; set; }
+            public Result result { get; set; }
         }
+
+        public class Result
+        {
+            public string resource_id { get; set; }
+            public int limit { get; set; }
+            public int total { get; set; }
+            public Field[] fields { get; set; }
+            public Record[] records { get; set; }
+        }
+
+        public class Field
+        {
+            public string type { get; set; }
+            public string id { get; set; }
+        }
+
+        public class Record
+        {
+            public string sno { get; set; }
+            public string sna { get; set; }
+            public string tot { get; set; }
+            public string sbi { get; set; }
+            public string sarea { get; set; }
+            public string mday { get; set; }
+            public string lat { get; set; }
+            public string lng { get; set; }
+            public string ar { get; set; }
+            public string sareaen { get; set; }
+            public string snaen { get; set; }
+            public string aren { get; set; }
+            public string bemp { get; set; }
+            public string act { get; set; }
+        }
+
     }
 }
